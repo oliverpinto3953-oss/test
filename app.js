2c283dc96b7c6cdba06d7a821eb1238bb0e37627
oliverpinto3953-oss <oliver.pinto3953@gmail.com>
2025-12-29 17:56:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBbUx3EGyj2p3J3Z2LVMABVLqqP1XIGaG4",
+
+  authDomain: "volunteer-aid-12ded.firebaseapp.com",
+
+  projectId: "volunteer-aid-12ded",
+
+  storageBucket: "volunteer-aid-12ded.firebasestorage.app",
+
+  messagingSenderId: "18674923750",
+
+  appId: "1:18674923750:web:c68d0ed4171dd9d3f0e327"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
 
